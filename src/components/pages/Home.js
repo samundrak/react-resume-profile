2fb2df001b0824fb6201ba009f52a46a1016fb59
samundrak <samundra.khatri@javra.com>
2018-03-07 18:08:32 +0545

    Support for dynamic content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Collapse } from 'antd';
+import PropTypes from 'prop-types';
+import camelCase from 'lodash.camelcase';
 import InfoIcon from '../../components/InfoIcon';
 import mappedComps from '../../mappedComps';
 
@@ -9,19 +11,29 @@ class Home extends React.Component {
   constructor() {
     super();
     this.state = {};
+    console.log(this.context);
   }
 
   render() {
+    const { home } = this.context.source;
     return (
       <Collapse bordered={false} defaultActiveKey={['About Me']}>
-        {mappedComps.map(item => (
-          <Panel header={<InfoIcon type={item.iconType} info={item.header} />} key={item.header}>
-            {item.component()}
-          </Panel>
-        ))}
+        {home
+          ? mappedComps.map(item => (
+            <Panel
+              header={<InfoIcon type={item.iconType} info={item.header} />}
+              key={item.header}
+            >
+              {item.component(home.tabs[camelCase(item.header)])}
+            </Panel>
+            ))
+          : ''}
       </Collapse>
     );
   }
 }
 
+Home.contextTypes = {
+  source: PropTypes.object, // eslint-disable0line
+};
 export default Home;
--- src/source.js
+++ src/source.js
@@ -8,9 +8,10 @@ export default {
     //   username: 'kardnumas',
     // },
   },
-  items: {
-    aboutMe: {
-      content: `
+  home: {
+    tabs: {
+      aboutMe: {
+        content: `
                 I am always striving to be broaden my knowledge and sharpen my skills. I love working in
                 environment with lots of challenges and brilliant people to share those challenges with. I am very
                 passionate about programming. I have keen eye for new paradigms and architecture. Though I am
@@ -18,18 +19,19 @@ export default {
                 my pet projects and participating in hackathons also i like to stay updated with the latest
                 technologies.
             `,
-    },
-    skills: {
-      items: [
-        {
-          language: 'javascript',
-          years: '3',
-        },
-      ],
-    },
-    experience: {},
-    resume: {
-      resource: 'https://www.pdf-archive.com/2018/03/05/samundra-kc-4/samundra-kc-4.pdf',
+      },
+      skills: {
+        items: [
+          {
+            language: 'javascript',
+            years: '3',
+          },
+        ],
+      },
+      experience: {},
+      resume: {
+        resource: 'https://www.pdf-archive.com/2018/03/05/samundra-kc-4/samundra-kc-4.pdf',
+      },
     },
   },
 };
