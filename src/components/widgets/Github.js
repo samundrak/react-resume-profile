4a00c0efd5e9b5ad5b1533b8a6ed6f8d61729e01
samundrak <samundrak@yahoo.com>
2018-03-09 00:53:18 +0545

    update commented code

--- src/components/widgets/Github.js
+++ src/components/widgets/Github.js
@@ -13,42 +13,9 @@ class Github extends React.Component {
     };
   }
   async componentDidMount() {
-    // this.setState({ loading: true });
-    // const profile = await axios.get(`https://api.github.com/users/${this.props.username}`);
-    // this.setState({ ...profile.data, loading: false });
-
-    this.setState({
-      login: 'samundrak',
-      id: 3079452,
-      avatar_url: 'https://avatars0.githubusercontent.com/u/3079452?v=4',
-      gravatar_id: '',
-      url: 'https://api.github.com/users/samundrak',
-      html_url: 'https://github.com/samundrak',
-      followers_url: 'https://api.github.com/users/samundrak/followers',
-      following_url: 'https://api.github.com/users/samundrak/following{/other_user}',
-      gists_url: 'https://api.github.com/users/samundrak/gists{/gist_id}',
-      starred_url: 'https://api.github.com/users/samundrak/starred{/owner}{/repo}',
-      subscriptions_url: 'https://api.github.com/users/samundrak/subscriptions',
-      organizations_url: 'https://api.github.com/users/samundrak/orgs',
-      repos_url: 'https://api.github.com/users/samundrak/repos',
-      events_url: 'https://api.github.com/users/samundrak/events{/privacy}',
-      received_events_url: 'https://api.github.com/users/samundrak/received_events',
-      type: 'User',
-      site_admin: false,
-      name: 'Samundra Khatri',
-      company: 'Javra Software',
-      blog: 'https://twitter.com/kardnumas',
-      location: 'Kathmandu , Nepal',
-      email: null,
-      hireable: true,
-      bio: ' ❤️ building stuffs with javascript 🍻 ',
-      public_repos: 87,
-      public_gists: 25,
-      followers: 80,
-      following: 229,
-      created_at: '2012-12-19T09:12:09Z',
-      updated_at: '2018-03-07T06:08:03Z',
-    });
+    this.setState({ loading: true });
+    const profile = await axios.get(`https://api.github.com/users/${this.props.username}`);
+    this.setState({ ...profile.data, loading: false });
   }
   render() {
     return (
